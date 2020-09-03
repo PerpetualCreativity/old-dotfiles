bb309780f97dca12c8609afef4a693bb336a8745
PerpetualCreativity <47309279+PerpetualCreativity@users.noreply.github.com>
2020-09-03 12:34:36 -0700

    os detection added to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,24 @@
+# TODO: detect platform, because karabiner/finicky is only mac
+# homebrew
+/bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+chsh -s $(which zsh)
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.vim ~/.vim
 
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
- 
-ln -s ~/.dotfiles/.finicky.js ~/.finicky.js
 
-ln -s ~/.dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
+pip install powerline-shell
+
+brew install neofetch
+
 ln -s ~/.dotfiles/neofetch.conf ~/.config/neofetch/config.conf
 ln -s ~/.dotfiles/powerline.json ~/.config/powerline-shell/config.json
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ln -s ~/.dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
+    ln -s ~/.dotfiles/.finicky.js ~/.finicky.js
+fi
 
