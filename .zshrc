aa0e34d18afea1ef986b1e6e7717572255dd8c45
PerpetualCreativity <47309279+PerpetualCreativity@users.noreply.github.com>
2020-11-11 08:15:43 -0800

    add more stuff

--- .zshrc
+++ .zshrc
@@ -1,38 +1,38 @@
-export PATH="/Users/ved/.local/bin:/usr/local/opt/libpq/bin:$PATH"
-
-function powerline_precmd() {
-	PS1="$(powerline-shell --shell zsh $?)"
-}
-function install_powerline_precmd() {
-  for s in "${precmd_functions[@]}"; do
-    if [ "$s" = "powerline_precmd" ]; then
-      return
-    fi
-  done
-  precmd_functions+=(powerline_precmd)
-}
-
-if [ "$TERM" != "linux" ]; then
-    install_powerline_precmd
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
+
+export PATH="/Users/ved/.local/bin:/usr/local/opt/libpq/bin:$PATH"
 
 [ -f "${GHCUP_INSTALL_BASE_PREFIX:=$HOME}/.ghcup/env" ] && source "${GHCUP_INSTALL_BASE_PREFIX:=$HOME}/.ghcup/env"
 
-# stop beeping at me :)
-unsetopt BEEP
-source /Users/ved/.zsh-plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+unsetopt BEEP # stop beeping at me :)
+
+source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme # powerlevel10k
+
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # zsh syntax highlighting
+
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh # zsh autosuggestions
+
+eval "$(zoxide init zsh)" # zoxide
 
 export PATH="/usr/local/opt/ruby/bin:$PATH"
 bindkey -v
 
-alias quit="exit"
+alias quit=exit
 alias py=python3
-
-alias n="nnn -e"
+alias todo=todo.sh
+alias cd=z
 
 # go stuff
 export GOPATH=$HOME/Go
 export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 # Simply comment the lines for things you don't want to install/run
+#
+# Things I haven't added yet:
+# menu-bar-splitter - https://github.com/jwhamilton99/menu-bar-splitter
 
 # homebrew
 /bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -15,9 +18,7 @@ ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 # link powerlevel10k config
-# if you ever feel like changing this, run
-# `p10k configure`
-# and the built-in wizard will guide you through
+# if you ever feel like changing this, run `p10k configure` and a config wizard will pop up
 brew install romkatv/powerlevel10k/powerlevel10k
 ln -s ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 
@@ -27,11 +28,16 @@ ln -s ~/.dotfiles/neofetch.conf ~/.config/neofetch/config.conf
 
 brew install ffmpeg # multimedia processing
 brew install zoxide # a smarter cd
-brew install todo-txt # simple+effective todo list utility
+brew install todo-txt; cp /usr/local/opt/todo-txt/todo.cfg ~/.todo.cfg  # simple+effective todo list utility
 brew install youtube-dl # for downloading videos
 brew install coreutils # install gnu coreutils
 brew install gh # github cli
 brew install so # stackoverflow/stackexchange cli
+brew install tldr # simple man pages
+
+brew install zsh-autosuggestions # autosuggestions for zsh
+brew install zsh-syntax-highlighting # syntax highlighting for zsh
+brew install zoxide # faster + fuzzier `cd`
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # powerful keyboard remapping
@@ -47,6 +53,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     brew cask install wireshark # network protocol analyser
     brew cask install obs # screen capturing/recording
     brew cask install michaelvillar-timer # small timer utility
+    brew cask install iglance # menu bar monitor
     # fonts! specifically, nerd fonts
     brew tap homebrew/cask-fonts
     brew cask install font-fira-code-nerd-font
@@ -54,4 +61,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     brew cask install font-source-code-pro-nerd-font
     brew cask install font-victor-mono-nerd-font
 fi
+
+source ~/.zshrc # force reload zsh
 
