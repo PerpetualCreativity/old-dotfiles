3d228b9496c1c6d4e52e9450b6397623c4f9b535
PerpetualCreativity <47309279+PerpetualCreativity@users.noreply.github.com>
2022-08-10 16:18:55 -0400

    update docs

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # PerpetualCreativity's dotfiles
-These are my dotfiles.
+These are my dotfiles. For macOS and Debian-based Linux distros only.
 
 ## Install
+
 0. `git clone https://github.com/PerpetualCreativity/dotfiles.git ~/.dotfiles`
-1. `zsh ~/.dotfiles/bootstrap.sh`
-2. `nvim ~/.config/nvim/init.vim` and then run `:PluginInstall`
+1. `~/.dotfiles/bootstrap.sh`
+2. restart: `sudo shutdown -r now`
+3. if there were errors during step 1, re-run `~/.dotfiles/bootstrap.sh`
+4. `nvim ~/.config/nvim/init.vim` and then run `:PluginInstall`
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-# homebrew
 if [ "$(uname -s)" = "Darwin" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     brew update
@@ -22,8 +21,8 @@ chsh -s $(which zsh)
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 # install the vim-plug plugin manager
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # neovim
 install neovim --HEAD
