a329d80e41ed7e7690d3f9eb41b855243b6da85f
PerpetualCreativity <47309279+PerpetualCreativity@users.noreply.github.com>
2023-01-29 21:06:09 -0500

    more keybinds!

--- switch-theme.zsh
+++ switch-theme.zsh
@@ -1,13 +1,17 @@
 #!/bin/zsh
 
 # determine theme to switch to
+# send usrsig1 to helix
 old='latte'
 new='mocha'
 
 sw () {
-  sed "s/${1/THEME/$old}/${helix/THEME/$new}/" $2 > $2
+  ex - $2 "+%s/${1/THEME/$old}/${1/THEME/$new}" "+x"
 }
 
 sw 'theme = "catppuccin_THEME"' ~/.config/helix/config.toml
+for pid in `pgrep hx`; do kill -USR1 $pid; done
+
 sw 'set -g @catppuccin_flavour '\''THEME'\' ~/.tmux.conf
+
 sw 'colors: *THEME' ~/.alacritty.yml
